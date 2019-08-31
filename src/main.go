a0ec72fd6e103cb0654c5ec6c28c79cbc454d307
bill <2229186680>
2019-09-01 00:36:24 +0800

    创建初始版本go 仓库

---
+++ src/main.go
@@ -0,0 +1,64 @@
+
+package main
+
+import "fmt"
+func main() {
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+
+	var a int
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+
+
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+	                                                              
+	var a int
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+
+	var a int
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+
+	var a int
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+
+	var a int
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+
+	var a int
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+
+	var a int
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+
+
+}
